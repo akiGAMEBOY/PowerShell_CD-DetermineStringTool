073120e5cafe21d0aec9614997931b83d8a0f80b
akiGAMEBOY <akigameboy@haretokidoki-blog.com>
2023-03-24 17:00:36 +0900

    master

--- Main.ps1
+++ Main.ps1
@@ -3,14 +3,14 @@
 # 機能　　｜CDドライブ内のファイルをチェックするツール
 #--------------------------------------------------------------------------------
 # 戻り値　｜下記の通り。
-# 　　　　｜   0：正常終了
-# 　　　　｜-101：設定ファイル読込みエラー
-# 　　　　｜-201：CDトレイのオープン（くり返し前の初回時）エラー
-# 　　　　｜-301：CDの挿入（セット）エラー
-# 　　　　｜-401：CDドライブの検証エラー
-# 　　　　｜-402：ファイル名の検証エラー
-# 　　　　｜-403：ファイル詳細の検証エラー
-# 　　　　｜-501：CDトレイのオープン（くり返し中の終了時）エラー
+# 　　　　｜   0: 正常終了
+# 　　　　｜-101: 設定ファイル読込みエラー
+# 　　　　｜-201: CDトレイのオープン（くり返し前の初回時）エラー
+# 　　　　｜-301: CDの挿入（セット）エラー
+# 　　　　｜-401: CDドライブの検証エラー
+# 　　　　｜-402: ファイル名の検証エラー
+# 　　　　｜-403: ファイル詳細の検証エラー
+# 　　　　｜-501: CDトレイのオープン（くり返し中の終了時）エラー
 # 引数　　｜-
 #################################################################################
 # 定数
@@ -27,7 +27,7 @@
 # 機能　　｜文字列を展開（先頭桁と最終桁にあるダブルクォーテーションを削除）
 #--------------------------------------------------------------------------------
 # 戻り値　｜String（展開後の文字列）
-# 引数　　｜target_str：対象文字列
+# 引数　　｜target_str: 対象文字列
 #################################################################################
 Function ExpandString($target_str) {
     [System.String]$expand_str = $target_str
@@ -47,8 +47,8 @@ Function ExpandString($target_str) {
 # 処理名　｜OpenCdtray
 # 機能　　｜CDトレイを自動でオープン
 #--------------------------------------------------------------------------------
-# 戻り値　｜Boolean（True：正常終了, False：異常終了）
-# 引数　　｜drive_full：対象ドライブ
+# 戻り値　｜Boolean（True: 正常終了, False: 異常終了）
+# 引数　　｜drive_full: 対象ドライブ
 #################################################################################
 Function OpenCdtray($drive_full) {
     [System.Boolean]$return = $false
@@ -62,7 +62,7 @@ Function OpenCdtray($drive_full) {
     }
     catch {
         $sbtemp=New-Object System.Text.StringBuilder
-        @("エラー　　：CDトレイ オープン処理`r`n",`
+        @("エラー　　: CDトレイ オープン処理`r`n",`
           "　　　　　　処理が失敗しました。`r`n")|
         ForEach-Object{[void]$sbtemp.Append($_)}
         $prompt_message = $sbtemp.ToString()
@@ -76,22 +76,22 @@ Function OpenCdtray($drive_full) {
 # 処理名　｜InsertCD
 # 機能　　｜CDトレイにCDメディアを手動で挿入（セット）
 #--------------------------------------------------------------------------------
-# 戻り値　｜Boolean（True：正常終了, False：処理中断）
-# 引数　　｜label：対象CDのラベル名
+# 戻り値　｜Boolean（True: 正常終了, False: 処理中断）
+# 引数　　｜label: 対象CDのラベル名
 #################################################################################
 Function InsertCD($label) {
     [System.Boolean]$return = $false
     [System.Text.StringBuilder]$sbtemp=New-Object System.Text.StringBuilder
     [System.String]$prompt_message = ''
 
-    @("捜査依頼　：CDのセット`r`n",`
+    @("捜査依頼　: CDのセット`r`n",`
       "　　　　　　処理を一時停止中。CDトレイに [ ${label} ] をセットしてください。`r`n")|
     ForEach-Object{[void]$sbtemp.Append($_)}
     $prompt_message = $sbtemp.ToString()
     Write-Host $prompt_message -ForegroundColor DarkYellow
     
     $sbtemp=New-Object System.Text.StringBuilder
-    @("確認　　　：処理続行の確認`r`n",`
+    @("確認　　　: 処理続行の確認`r`n",`
       "　　　　　　CDをトレイに入れた後に応答し再開してください。処理を再開しますか？`r`n",`
       "　　　　　　[ y: はい、n: いいえ ]`r`n",`
       "`r`n",`
@@ -109,8 +109,8 @@ Function InsertCD($label) {
 # 処理名　｜ConfirmYesno
 # 機能　　｜YesNo入力
 #--------------------------------------------------------------------------------
-# 戻り値　｜Boolean（True：正常終了, False：処理中断）
-# 引数　　｜prompt_message：入力応答待ち時のメッセージ内容
+# 戻り値　｜Boolean（True: 正常終了, False: 処理中断）
+# 引数　　｜prompt_message: 入力応答待ち時のメッセージ内容
 #################################################################################
 Function ConfirmYesno($prompt_message) {
     [System.Boolean]$return = $false
@@ -135,7 +135,7 @@ Function ConfirmYesno($prompt_message) {
         elseif ($value.ToLower() -eq "n") {
             $return = $false
             $sbtemp=New-Object System.Text.StringBuilder
-            @("エラー　　：いいえを選択`r`n", `
+            @("エラー　　: いいえを選択`r`n", `
               "　　　　　　処理を中断します。`r`n")|
             ForEach-Object{[void]$sbtemp.Append($_)}
             $prompt_message = $sbtemp.ToString()
@@ -145,7 +145,7 @@ Function ConfirmYesno($prompt_message) {
         elseif ($i -eq $c_retry_count) {
             $return = $false
             $sbtemp=New-Object System.Text.StringBuilder
-            @("エラー　　：リトライ回数を超過`r`n", `
+            @("エラー　　: リトライ回数を超過`r`n", `
               "　　　　　　リトライ回数（", `
               [System.String]$c_retry_count, `
               "回）を超過した為、処理を中断します。`r`n")|
@@ -162,8 +162,8 @@ Function ConfirmYesno($prompt_message) {
 # 処理名　｜ValidateDrive
 # 機能　　｜CDドライブの検証
 #--------------------------------------------------------------------------------
-# 戻り値　｜Boolean（True：正常終了, False：異常終了）
-# 引数　　｜drive：対象ドライブ（ドライブレターのみ）, drive_full：対象ドライブ
+# 戻り値　｜Boolean（True: 正常終了, False: 異常終了）
+# 引数　　｜drive: 対象ドライブ（ドライブレターのみ）, drive_full: 対象ドライブ
 #################################################################################
 Function ValidateDrive($drive) {
     [System.Boolean]$return = $false
@@ -182,7 +182,7 @@ Function ValidateDrive($drive) {
             if ($itemlist.Count -ge 1) {
                 $return = $true
                 $sbtemp=New-Object System.Text.StringBuilder
-                @("通知　　　：CDドライブの検証`r`n",`
+                @("通知　　　: CDドライブの検証`r`n",`
                   "　　　　　　正常にCDドライブを認識しました。`r`n")|
                 ForEach-Object{[void]$sbtemp.Append($_)}
                 $prompt_message = $sbtemp.ToString()
@@ -192,7 +192,7 @@ Function ValidateDrive($drive) {
             else {
                 $is_exists = $true
                 $sbtemp=New-Object System.Text.StringBuilder
-                @("エラー　　：CDドライブの検証`r`n",`
+                @("エラー　　: CDドライブの検証`r`n",`
                   "　　　　　　CDドライブ内のデータがありませんでした。`r`n",`
                   "　　　　　　処理を中断します。`r`n")|
                 ForEach-Object{[void]$sbtemp.Append($_)}
@@ -205,7 +205,7 @@ Function ValidateDrive($drive) {
         Start-Sleep $c_wait_sec
         $now = $c_wait_sec * $i
         $sbtemp=New-Object System.Text.StringBuilder
-        @("通知　　　：CDドライブの検証`r`n",`
+        @("通知　　　: CDドライブの検証`r`n",`
           "　　　　　　チェック中。　経過時間 / 待ち時間 [ ${now} / ${max} sec ]`r`n",`
           "　　　　　　CDを認識するまで少々、お待ちください。`r`n")|
         ForEach-Object{[void]$sbtemp.Append($_)}
@@ -215,7 +215,7 @@ Function ValidateDrive($drive) {
     # 待ち合わせたが、認識できなかった場合
     if ((-not $return) -And (-not $is_exists)) {
         $sbtemp=New-Object System.Text.StringBuilder
-        @("エラー　　：CDドライブの検証`r`n",`
+        @("エラー　　: CDドライブの検証`r`n",`
           "　　　　　　CDを認識できませんでした。`r`n",`
           "　　　　　　処理を中断します。`r`n")|
         ForEach-Object{[void]$sbtemp.Append($_)}
@@ -230,8 +230,8 @@ Function ValidateDrive($drive) {
 # 処理名　｜ValidateFileformat
 # 機能　　｜ファイル形式（ファイル名と拡張子）の検証
 #--------------------------------------------------------------------------------
-# 戻り値　｜Boolean（True：正常終了, False：異常終了）
-# 引数　　｜drive_full：対象ドライブ
+# 戻り値　｜Boolean（True: 正常終了, False: 異常終了）
+# 引数　　｜drive_full: 対象ドライブ
 #################################################################################
 Function ValidateFileformat($drive_full) {
     [System.Boolean]$return = $false
@@ -241,16 +241,16 @@ Function ValidateFileformat($drive_full) {
     [Object[]]$itemlist = Get-ChildItem "${drive_full}" | Sort-Object {$_.Name}
     foreach($item in $itemlist) {
         # ファイル名が下記の通りである事を検証
-        #   1～4桁目　　：アルファベット（大文字・小文字を区別しない）
-        #   ファイル種類：csvファイル or テキストファイル
+        #   1～4桁目　　: アルファベット（大文字・小文字を区別しない）
+        #   ファイル種類: csvファイル or テキストファイル
         if (-not(($item.Name -match '^[A-z][A-z][A-z][A-z]') -And `
             (($item.Name.ToLower() -match '\.csv$') -Or `
              ($item.Name.ToLower() -match '\.txt$')))) {
             $return = $false
             $sbtemp=New-Object System.Text.StringBuilder
-            @("エラー　　：ファイル名の検証`r`n",`
+            @("エラー　　: ファイル名の検証`r`n",`
               "　　　　　　既定のファイル名ではありません。`r`n",`
-              "　　　　　　対象ファイル：[$($item.FullName)]`r`n")|
+              "　　　　　　対象ファイル: [$($item.FullName)]`r`n")|
             ForEach-Object{[void]$sbtemp.Append($_)}
             $prompt_message = $sbtemp.ToString()
             Write-Host $prompt_message -ForegroundColor DarkRed
@@ -265,8 +265,8 @@ Function ValidateFileformat($drive_full) {
 # 処理名　｜ValidateFiledetail
 # 機能　　｜ファイル内の検証（文字列の有無を判定）
 #--------------------------------------------------------------------------------
-# 戻り値　｜Boolean（True：正常終了, False：異常終了）
-# 引数　　｜drive_full：対象ドライブ, findrange：検索範囲, findstring：検索文字列
+# 戻り値　｜Boolean（True: 正常終了, False: 異常終了）
+# 引数　　｜drive_full: 対象ドライブ, findrange: 検索範囲, findstring: 検索文字列
 #################################################################################
 Function ValidateFiledetail($drive_full, $findrange, $findstring) {
     [System.Boolean]$return = $false
@@ -288,10 +288,10 @@ Function ValidateFiledetail($drive_full, $findrange, $findstring) {
         if ($compared.Count -ge 1) {
             $return = $true
             $sbtemp=New-Object System.Text.StringBuilder
-            @("通知　　　：ファイルの詳細を検証`r`n",`
-              "　　　　　　検証結果：成功（$(($without_ext))）`r`n",`
+            @("通知　　　: ファイルの詳細を検証`r`n",`
+              "　　　　　　検証結果: 成功（$(($without_ext))）`r`n",`
               "　　　　　　検索の文字列「$(($findstring))」が見つかりました。`r`n",`
-              "　　　　　　対象：[$($item.FullName)]`r`n")|
+              "　　　　　　対象: [$($item.FullName)]`r`n")|
             ForEach-Object{[void]$sbtemp.Append($_)}
             $prompt_message = $sbtemp.ToString()
             Write-Host $prompt_message -ForegroundColor Blue
@@ -299,10 +299,10 @@ Function ValidateFiledetail($drive_full, $findrange, $findstring) {
         else {
             $return = $false
             $sbtemp=New-Object System.Text.StringBuilder
-            @("エラー　　：ファイルの詳細を検証`r`n",`
-              "　　　　　　検証結果：失敗（$(($without_ext))）`r`n",`
+            @("エラー　　: ファイルの詳細を検証`r`n",`
+              "　　　　　　検証結果: 失敗（$(($without_ext))）`r`n",`
               "　　　　　　検索の文字列「$(($findstring))」が見つかりませんでした。`r`n",`
-              "　　　　　　対象：[$($item.FullName)]`r`n")|
+              "　　　　　　対象: [$($item.FullName)]`r`n")|
             ForEach-Object{[void]$sbtemp.Append($_)}
             $prompt_message = $sbtemp.ToString()
             Write-Host $prompt_message -ForegroundColor DarkRed
@@ -316,7 +316,7 @@ Function ValidateFiledetail($drive_full, $findrange, $findstring) {
 # 処理名　｜ConfirmLoop
 # 機能　　｜くり返し確認
 #--------------------------------------------------------------------------------
-# 戻り値　｜Boolean（True：繰り返す, False：繰り返し終了）
+# 戻り値　｜Boolean（True: 繰り返す, False: 繰り返し終了）
 # 引数　　｜-
 #################################################################################
 Function ConfirmLoop() {
@@ -327,7 +327,7 @@ Function ConfirmLoop() {
     $return = $true
 
     # くり返し有無を確認
-    @("確認　　　：くり返し有無の確認`r`n",`
+    @("確認　　　: くり返し有無の確認`r`n",`
       "　　　　　　処理が終了しました。続けて処理しますか？`r`n",`
       "　　　　　　[ y: はい、n: いいえ ]`r`n",`
       "`r`n",`
@@ -379,9 +379,9 @@ try {
     [System.String]$findstring = ExpandString($param.FindString)
 
     $sbtemp=New-Object System.Text.StringBuilder
-    @("通知　　　：設定ファイル読み込み`r`n",`
+    @("通知　　　: 設定ファイル読み込み`r`n",`
       "　　　　　　設定ファイルの読み込みが正常終了しました。`r`n",`
-      "　　　　　　対象：[${config_fullpath}]`r`n")|
+      "　　　　　　対象: [${config_fullpath}]`r`n")|
     ForEach-Object{[void]$sbtemp.Append($_)}
     $prompt_message = $sbtemp.ToString()
     Write-Host $prompt_message
@@ -389,9 +389,9 @@ try {
 catch {
     $result = -101
     $sbtemp=New-Object System.Text.StringBuilder
-    @("エラー　　：設定ファイル読み込み`r`n",`
+    @("エラー　　: 設定ファイル読み込み`r`n",`
       "　　　　　　設定ファイルの読み込みが異常終了しました。`r`n",`
-      "　　　　　　エラー内容：[${config_fullpath}",`
+      "　　　　　　エラー内容: [${config_fullpath}",`
     "$($_.Exception.Message)]`r`n")|
     ForEach-Object{[void]$sbtemp.Append($_)}
     $result_message = $sbtemp.ToString()
@@ -459,15 +459,15 @@ for ($count = 1; $count -le $c_for_count; $count++) {
     # 処理結果の表示
     $sbtemp=New-Object System.Text.StringBuilder
     if ($result -eq 0) {
-        @("処理結果　：正常終了`r`n",`
-          "　　　　　　メッセージコード：[${result}]`r`n")|
+        @("処理結果　: 正常終了`r`n",`
+          "　　　　　　メッセージコード: [${result}]`r`n")|
         ForEach-Object{[void]$sbtemp.Append($_)}
         $result_message = $sbtemp.ToString()
         Write-Host $result_message
     }
     else {
-        @("処理結果　：異常終了`r`n",`
-          "　　　　　　メッセージコード：[${result}]`r`n")|
+        @("処理結果　: 異常終了`r`n",`
+          "　　　　　　メッセージコード: [${result}]`r`n")|
         ForEach-Object{[void]$sbtemp.Append($_)}
         $result_message = $sbtemp.ToString()
         Write-Host $result_message -ForegroundColor DarkRed
@@ -484,3 +484,6 @@ for ($count = 1; $count -le $c_for_count; $count++) {
         break
     }
 }
+
+# 終了
+exit $result
